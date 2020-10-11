709c61e35301130e433d9f9fc6d18924ca425b06
Micah <micahjank@gmail.com>
2020-10-11 16:47:55 -0500

    testing

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ db = SQLAlchemy(app)
 
 # the models can be created here, this is similar to what happense in node.js/express when creating migrations and 
 # defining what the tables/columns/rows are going to be
-class VideoModel(db.Model):
+class VideoModel(db.Model): 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     views = db.Column(db.Integer, nullable=False)
